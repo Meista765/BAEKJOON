04b033b769eae5bdf2bca80cd2498b139dec8daf
조현준 <wns950404@naver.com>
2024-07-23 17:15:29 +0900

    2024-07-23,17:15

--- 12891.py
+++ 12891.py
@@ -7,35 +7,83 @@
 import sys
 input = sys.stdin.readline
 
-S, P = map(int, input().split())
-DNA = input().rstrip()
-cond = list(map(int, input().split()))
+# Global variables
+S, P = map(int, input().split()) # 문자열 크기, 윈도우 크기
+A = input() # 문자열 데이터
+count = 0 # 결과를 저장하는 변수(int)
 
-# 정답 초기화
-cnt = 0
+check_list = [] # int list; 비밀번호 체크 리스트
+my_list = [0] * 4 # int list; 현재 상태 리스트
+achieved = 0 # int; 충족 조건 개수를 저장하는 변수
 
-# 부분 문자열이 보유한 A,C,G,T 초기화
-now = {'A': DNA[0:P].count('A'),
-       'C': DNA[0:P].count('C'), 
-       'G': DNA[0:P].count('G'), 
-       'T': DNA[0:P].count('T')}
+# Define functions
+def my_add(dna:str):
+    # my_list에 새로운 값을 더하고, 조건에 따라 achieved 확인
+    global achieved, my_list, check_list
 
-for i in range(0, S-P+1):
-    # 부분 문자열 추출
-    partial = DNA[i : i+P]
-    
-    # 부분 문자열 조건 확인
-    is_ok = True
-    for a, b in zip(now.keys(), cond):
-        is_ok *= (a < b)
-    
-    # 조건 확인 성공시
-    if is_ok:
-        cnt += 1
-    
-    # ACGT 갱신
-    now[partial[i]] -= 1
-    if i+P
+    if dna == 'A':
+        my_list[0] += 1
+        if my_list[0] == check_list[0]:
+            achieved += 1
+    elif dna == 'C':
+        my_list[1] += 1
+        if my_list[1] == check_list[1]:
+            achieved += 1
+    elif dna == 'G':
+        my_list[2] += 1
+        if my_list[2] == check_list[2]:
+            achieved += 1
+    elif dna == 'T':
+        my_list[3] += 1
+        if my_list[3] == check_list[3]:
+            achieved += 1
 
 
-print(cnt)
+def my_remove(dna:str):
+    # my_list에 새로운 값을 제거하고 조건에 따라 achieved update
+    global achieved, my_list, check_list
+
+    if dna == 'A':
+        my_list[0] -= 1
+        if my_list[0] == check_list[0] - 1:
+            achieved -= 1
+    elif dna == 'C':
+        my_list[1] -= 1
+        if my_list[1] == check_list[1] - 1:
+            achieved -= 1
+    elif dna == 'G':
+        my_list[2] -= 1
+        if my_list[2] == check_list[2] - 1:
+            achieved -= 1
+    elif dna == 'G':
+        my_list[3] -= 1
+        if my_list[3] == check_list[3] - 1:
+            achieved -= 1
+
+# main
+
+# 1. check_list 데이터 받기
+check_list = list(map(int, input().split()))
+
+# 2. check_list를 탐색하여 값이 0인 데이터의 개수만큼 achieved 값 증가
+for i in range(4):
+    if check_list[i] == 0:
+        achieved += 1
+
+# 3. 최초 데이터의 유효성 판단
+for i in range(P): # 0 ~ P-1
+    my_add(A[i])
+
+if achieved == 4:
+    count += 1
+
+# 4. P ~ S-1까지 탐색
+for new in range(P, S):
+    old = new - P
+    my_remove(old)
+    my_add(new)
+    if achieved == 4:
+        count += 1
+
+# 5. 결과 출력
+print(count)
