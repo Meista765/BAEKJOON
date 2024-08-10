7009999f8df8e7d858ad179f7ec6942bcd36c218
조현준 <wns950404@naver.com>
2024-08-10 23:26:50 +0900

    2024-08-10,23:26

--- 01167.py
+++ 01167.py
@@ -5,31 +5,46 @@ from collections import deque
 
 V = int(input())
 
-def find_radius():
+def BFS(start):
+    # 방문 리스트 초기화
+    visited = [0] * (V+1)
+    
+    # 최대 거리 저장
+    distance = [0] * (V+1)
+    
+    # 반환값: 최대거리와 그 인덱스
+    max_dist = -1
+    max_idx = -1
+    
     queue = deque()
     
-    distance = [0] * (V+1)
-    for i in range(1, V+1):
-        if adj[i]:
-            visited[i] = 1
-            for data in adj[i]:
-                visited[data[0]] = 1
-                distance[data[0]] = data[1]
-                queue.append(data)
-            break
+    # 최초값 push
+    visited[start] = 1
+    queue.append(start)    
     
+    # 큐가 빌 때까지 위 작업을 반복
     while queue:
+        v1 = queue.pop()
         
-        
-        for 
+        # v2: 목적지 정점, w: 가중치
+        for data in adj[v1]:
+            v2, w = data
+            if not visited[v2]:
+                visited[v2] = 1
+                distance[v2] = distance[v1] + w
+                if distance[v2] > max_dist:
+                    max_idx = v2
+                    max_dist = distance[v2]
+                queue.append(v2)
     
-
+    # test용 코드
+    # print(distance[1:])
+    # print('max_idx:', max_idx, ', max_dist:', max_dist)
+    return (max_idx, max_dist)
 
 # 인접행렬 데이터 구조: (목적지, 가중치)
 adj = [[] for _ in range(V+1)]
 
-# 방문 리스트
-visited = [0] * (V+1)
 
 # 인접 행렬 제작
 for _ in range(V):
@@ -42,3 +57,17 @@ for _ in range(V):
         
         adj[v1].append((v2, weight))
 
+
+# 임의의 시작 위치 선정
+tmp_start = int()
+for i in range(1, V+1):
+    if adj[i]:
+        tmp_start = i
+        break
+
+# 1회 실행하여 실제 시작 위치를 구함(임의의 정점에서 거리가 가장 먼 정점)
+real_start, dummy = BFS(tmp_start)
+# 실제 시작 위치에서 가장 먼 정점을 계산
+dummy, answer = BFS(real_start)
+
+print(answer)
--- 02023.py
+++ 02023.py
@@ -1,13 +1,44 @@
 import sys
-input = sys.stdin.readline
 
-import math
+input = sys.stdin.readline
+print = sys.stdout.write
+
+prime_number = {2, 3, 5, 7}
+# 소수인지 판별하는 함수
+def is_prime_number(tgt:int) -> bool:
+    # memorization 활용
+    if tgt in prime_number:
+        return True
+    
+    end = int(tgt ** (1/2))
+    for i in range(2, end+1):
+        if tgt % i == 0:
+            return False
+    
+    prime_number.add(tgt)
+    return True
+
+def solve(tgt_digit:int, curr_digit:int, chk_num:int):
+    '''
+    :tgt_digit: 목표로 하는 자리수
+    :curr_digit: 현재 검토중인 자리수, 재귀의 깊이를 나타냄
+    :chk_num: 현재 검토중인 수
+    '''
+    # 현재 검토중인 수가 소수가 아니라면 pruning
+    if not is_prime_number(chk_num):
+        return
+    else:  # 소수라면
+        # 목표 자리수보다 깊이 들어왔다면 return
+        if curr_digit > tgt_digit:
+            return
+        # 목표 자리수와 동일하다면 
+        elif curr_digit == tgt_digit:
+            print(str(chk_num) + '\n')
+        else: # 목표 자리수보다 작다면
+            for i in range(1, 10, 2): # 1, 3, 5, 7, 9
+                solve(tgt_digit, curr_digit+1, chk_num * 10 + i)
 
 N = int(input())
-P = [True] * 1e+9  # 소수
 
-def Eratos(digit):
-    for i in range(2, math.sqrt(10 ** digit) + 1):
-        if P[i] == True:
-            while i < 10 ** digit:
-                
+for i in [2, 3, 5, 7]:
+    solve(N, 1, i)
